76c11f209790ebd2fc4ebdd522d0285bf43b20be
Anubhav Utkarsh <au0055@srmist.edu.in>
2023-07-03 11:26:55 +0530

    update

--- ML/r^2ed.py
+++ ML/r^2ed.py
@@ -7,7 +7,7 @@ y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
 
 mymodel = np.poly1d(np.polyfit(x,y,3)) # 3 is the degree of the polynomial
 myline = np.linspace(1,22,100)
-speed = mymodel(4)
+speed = mymodel(5)
 plt.scatter(x,y)
 plt.plot(myline, mymodel(myline))
 plt.show()
