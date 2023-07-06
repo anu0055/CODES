937ca696912754f7f53b612a0f085bf75bbc73c4
Anubhav Utkarsh <au0055@srmist.edu.in>
2023-07-06 13:02:23 +0530

    python

---
+++ Python/normal_dist.py
@@ -0,0 +1,7 @@
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+x = random.normal(loc = 10, scale= 2, size = 100)
+print(x)
+sns.distplot(x, hist=False)
+plt.show()
